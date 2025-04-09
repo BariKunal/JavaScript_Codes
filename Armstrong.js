b4d9c031f2d04bdb5f078cfce31ff109d1b099eb
BariKunal <kunalbari751@gmail.com>
2025-04-09 22:02:46 +0530

    sum calculated

--- Armstrong.js
+++ Armstrong.js
@@ -1,10 +1,10 @@
 var num = 153
 var temp = num;
 var sum =0
-while(num!=0){
+while(num>0){
     let rem = num % 10;
         sum = sum + (rem*rem*rem)
-        num = num /10;
+        num = num / 10;
 }
 console.log(temp)
 console.log(sum)
