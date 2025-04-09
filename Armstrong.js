984800a80ef9b8005d0f0f55410f84f01e772297
BariKunal <kunalbari751@gmail.com>
2025-04-09 22:07:16 +0530

    calculated

--- Armstrong.js
+++ Armstrong.js
@@ -1,13 +1,12 @@
 var num = 153
 var temp = num;
 var sum =0
-while(num>0){
+for(;num>0;){
     let rem = num % 10;
         sum = sum + (rem*rem*rem)
-        num = num / 10;
+        num = Math.floor(num / 10);
 }
-console.log(temp)
-console.log(sum)
+
 if(sum==temp){
     console.log("Armstrong Number")
 }
