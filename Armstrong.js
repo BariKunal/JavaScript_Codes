cf8ef161ffcc98442bfe742ab3d3a815f7c1404e
BariKunal <kunalbari751@gmail.com>
2025-04-09 22:10:32 +0530

    implements for 4/5 digit number

--- Armstrong.js
+++ Armstrong.js
@@ -1,9 +1,13 @@
-var num = 153
+var num = 9474
 var temp = num;
 var sum =0
+
+var digit = num.toString().length;
+
 for(;num>0;){
     let rem = num % 10;
-        sum = sum + (rem*rem*rem)
+        // sum = sum + (rem*rem*rem)
+        sum += Math.pow(rem,digit)
         num = Math.floor(num / 10);
 }
 
