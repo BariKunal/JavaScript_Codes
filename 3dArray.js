8abcedd3842cfab5ca541e0b4e827ac25f24306d
BariKunal <kunalbari751@gmail.com>
2025-04-12 22:20:39 +0530

    commit

--- 3dArray.js
+++ 3dArray.js
@@ -2,5 +2,5 @@ var arr=[[[1,2],[3,4]],[[5,6],[7,8]],[[9,1],[1,1]]];
 arr.forEach(show)
 
 function show(value,index,arr){
-    console.log(index +"\t"+ value)
+    console.log(index +"\t"+ value);
 }
