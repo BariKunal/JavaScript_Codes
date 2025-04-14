8efcb092bc1d311b58e748b9f6d7a49a855308e5
BariKunal <kunalbari751@gmail.com>
2025-04-14 20:17:58 +0530

    functions with sub

--- functions.js
+++ functions.js
@@ -3,3 +3,9 @@ function show(a,b){
 }
 let res = show(8,9);
 console.log("Addition Is :", res)
+
+function show1(a,b){
+    return a-b;
+}
+let resu = show1(18,9);
+console.log("Subtraction Is :", resu)
