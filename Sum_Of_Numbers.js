4f4a0091eb965bc46479043fdc268fe726d74973
BariKunal <kunalbari751@gmail.com>
2025-04-16 18:35:06 +0530

    Sum of numbers using while loop

---
+++ Sum_Of_Numbers.js
@@ -0,0 +1,14 @@
+
+// 1. Sum of Numbers
+// Description: Write a program to calculate the sum of numbers from 1 to n.
+// Input: n = 5
+// Output: 15
+
+let num = 5;
+let sum = 0; 
+let i=1;
+while(i<=num){
+    sum = sum+i;
+    i++;
+}
+console.log("Sum Of Numbers from "+num+" IS :"+sum);
