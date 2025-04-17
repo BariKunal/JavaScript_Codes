e08a1a9add26012bb4ddd2badd1e9af2db515d86
BariKunal <kunalbari751@gmail.com>
2025-04-17 23:10:37 +0530

    Odd Numbers

---
+++ Looping Programs/Odd_Numbers.js
@@ -0,0 +1,11 @@
+// 12. Odd Numbers
+// Description: Write a program to print all odd numbers between 1 and n.
+// Input: n = 10
+// Output: 1 3 5 7 9
+
+let num = 10
+for (let i = 1; i <= num; i++){
+    if(i%2===1){
+        console.log(i)
+    }
+}
