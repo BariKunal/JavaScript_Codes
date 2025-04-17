a84c3ed15494bf04729c7c13087504c832d4af14
BariKunal <kunalbari751@gmail.com>
2025-04-17 13:52:26 +0530

    Multiplication Table

---
+++ Looping Programs/Multiplication_Table.js
@@ -0,0 +1,12 @@
+// 8. Multiplication Table
+// Description: Write a program to print the multiplication table for a number n.
+// Input: n = 4
+
+// Output: 4 8 12 16 20
+
+let num = 10;
+let i=1;
+while(i<=num){
+    console.log(i,"*",num,"=",num*i)
+    i++
+}
