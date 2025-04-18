2226340eafa909798a8355bd08d47ec9ffbb62d1
BariKunal <kunalbari751@gmail.com>
2025-04-18 13:34:25 +0530

    Reverse Number Using revese inbuild method

---
+++ Arrays_Js/Reverse_Num.js
@@ -0,0 +1,10 @@
+// 1. Reverse an Array
+// Description: Reverse the order of elements in an array.
+// Input: [1, 2, 3, 4, 5]
+
+// Output: [5, 4, 3, 2, 1]
+
+let arr=[1,2,3,4,5];
+
+let res = arr.reverse()
+console.log(res);
