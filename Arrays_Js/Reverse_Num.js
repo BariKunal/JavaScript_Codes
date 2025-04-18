a670d4f8fee2ab420aa3d98d8b52f4bbfcb31603
BariKunal <kunalbari751@gmail.com>
2025-04-18 13:40:25 +0530

    Reverse an Array without inbuild method

--- Arrays_Js/Reverse_Num.js
+++ Arrays_Js/Reverse_Num.js
@@ -6,5 +6,18 @@
 
 let arr=[1,2,3,4,5];
 
-let res = arr.reverse()
-console.log(res);
+// let res = arr.reverse()
+// console.log(res);
+
+let end = arr.length-1;
+let mid = arr.length/2;
+for (let i = 0; i < mid; i++) {
+    let temp = arr[i]
+        arr[i] = arr[end]
+        arr[end] = temp
+        end--
+}
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+    
+}
