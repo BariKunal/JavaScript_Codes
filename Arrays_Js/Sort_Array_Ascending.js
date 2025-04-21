2bb92054287c26190bf22b6267fdb3f2c9c33bfe
BariKunal <kunalbari751@gmail.com>
2025-04-21 15:12:34 +0530

    Sort the elements of the array in ascending order.

---
+++ Arrays_Js/Sort_Array_Ascending.js
@@ -0,0 +1,21 @@
+// 13. Sort Array in Ascending Order
+// Description: Sort the elements of the array in ascending order.
+
+// Input: [10, 2, 30, 4, 5]
+
+// Output: [2, 4, 5, 10, 30]
+
+let a = [10, 2, 30, 4, 5]
+for (let i = 0; i < a.length-1; i++) {
+    for (let j = i+1; j < a.length; j++) {
+        if (a[i]>a[j]) {
+            let temp = a[i]
+            a[i] = a[j]
+            a[j] = temp
+        }
+    }
+}
+for (let i = 0; i < a.length; i++) {
+    
+    console.log(a[i]);
+}
