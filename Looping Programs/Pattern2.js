044da67f10f04b2981c778710da0dd3238491b68
BariKunal <kunalbari751@gmail.com>
2025-04-22 11:54:48 +0530

    Pattern 2 in Js

---
+++ Looping Programs/Pattern2.js
@@ -0,0 +1,11 @@
+for (let i = 1; i <= 5; i++) {
+    let str = ""
+    for (let j = 1; j <= 5; j++) {
+        if (i <= j) {
+            str = str + "* "
+        }
+    }
+    console.log(str);
+
+
+}
