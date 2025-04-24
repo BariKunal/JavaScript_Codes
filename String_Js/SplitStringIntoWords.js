e7f5d53aa3f2d42633112d0dddeac24710924a90
BariKunal <kunalbari751@gmail.com>
2025-04-24 12:33:56 +0530

    Split a string into words.

---
+++ String_Js/SplitStringIntoWords.js
@@ -0,0 +1,11 @@
+// 33. Split a String into Words
+// Description: Split a string into words.
+
+// Input: "hello world"
+
+// Output: ["hello", "world"]
+
+let str = "hello world"
+let emptyStr = str.trim().split(' ');
+
+console.log(emptyStr);
