230f55fe89977591254b91781bb598c7e3e5bffe
BariKunal <kunalbari751@gmail.com>
2025-04-28 23:27:47 +0530

    JavaScript file linked

---
+++ Basic_Projects-Js/Todo List/todo.js
@@ -0,0 +1,7 @@
+function AddText(){
+    let inputTag = document.getElementById("txtTag")
+    let ul = document.getElementById("addTask");
+    if (inputTag.value === '') {
+        alert("Add First")
+    }
+}
