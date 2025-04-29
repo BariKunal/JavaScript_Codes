74f70a3fba37f877482a06c55a0857a49e931ac2
BariKunal <kunalbari751@gmail.com>
2025-04-29 23:50:55 +0530

    JavaScript Function Add

--- Basic_Projects-Js/Todo List/todo.js
+++ Basic_Projects-Js/Todo List/todo.js
@@ -1,7 +1,30 @@
-function AddText(){
+function AddText() {
     let inputTag = document.getElementById("txtTag")
     let ul = document.getElementById("addTask");
-    if (inputTag.value === '') {
-        alert("Add First")
+    if (inputTag.value === "") {
+        alert("Add Text First")
+    }
+    else{
+
+        let textDiv = document.createElement("div");
+        textDiv.style.height="40px"
+        textDiv.style.width="330px"
+        textDiv.style.border="1px solid black"
+        textDiv.style.display="flex"
+        textDiv.style.justifyContent="space-between"
+        textDiv.style.alignItems="center"
+    
+        let btnRemove = document.createElement("button")
+        btnRemove.innerText="Remove"
+        btnRemove.style.height="40px"
+        btnRemove.style.width="70px"
+        textDiv.innerHTML=inputTag.value;
+        textDiv.appendChild(btnRemove)
+        ul.appendChild(textDiv)
+        inputTag.value = ""
+    
+        btnRemove.addEventListener("click", function(){
+            textDiv.remove()
+        })
     }
 }
